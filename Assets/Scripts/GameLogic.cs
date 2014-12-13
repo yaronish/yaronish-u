5cde3317956fb7da354f50c79fc1b98ab58de28f
yonischenko <yar.onish@gmail.com>
2014-12-13 13:38:46 +0200

    Squares game

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GameLogic : MonoBehaviour {
+
+	private List<GameObject> objects = new List<GameObject>();
+
+	private int rowsNum = 3;
+
+	private int colsNum = 3;
+
+	public GameObject squarePrefab;
+
+	// Use this for initialization
+	void Start () 
+	{
+		for (int i = 0; i < rowsNum; i++) {
+			for (int j = 0; j < colsNum; j++) {
+				GameObject square = Instantiate(squarePrefab, new Vector3(i, j, 0f), Quaternion.identity) as GameObject;
+				objects.Add(square);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
--- Assets/Standard Assets/Scripts/General Scripts/DragRigidbody.js
+++ Assets/Standard Assets/Scripts/General Scripts/DragRigidbody.js
@@ -1,4 +1,4 @@
-var spring = 50.0;
+var spring = 2000.0;
 var damper = 5.0;
 var drag = 10.0;
 var angularDrag = 5.0;
